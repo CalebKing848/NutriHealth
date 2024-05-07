d91c7efbb910fe37818f13f9d0478c392601db01
CalebKing848 <calebking848@outlook.com>
2024-05-07 13:04:42 +1200

    Upload models

--- NutriHealth/NutritionApp/urls.py
+++ NutriHealth/NutritionApp/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
 
     path('register', views.register, name='register'), # Register Page
+
+    path('user-infromation', views.user_infromation, name='user-infromation'), # User Infromation 
+
 ]
