81bf1dad766c02d5b9b5c6ed98f49c14eff62440
CalebKing848 <calebking848@outlook.com>
2024-05-09 10:53:24 +1200

    added contact us page

--- NutriHealth/NutritionApp/forms.py
+++ NutriHealth/NutritionApp/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 from .models import UserInformation
+from .models import ContactInformation
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -16,3 +17,8 @@ class UserInformationForm(forms.ModelForm):
     class Meta:
         model = UserInformation
         fields = ["age", "gender", "weight", "height", "energy_intake", "existing_medical_conditions"]
+
+class ContactInformationForm(forms.ModelForm):
+    class Meta:
+        model = ContactInformation
+        fields = ["name", "email", "message"]
